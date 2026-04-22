3e0958047c0a55f1c1710357d464c6e792c9dd92
bsteve <bicosteve4@gmail.com>
2026-04-22 07:59:54 +0300

    added resulting consumer

--- src/main/java/com/bicosteve/api_gateway/Main.java
+++ src/main/java/com/bicosteve/api_gateway/Main.java
@@ -1,11 +1,28 @@
 package com.bicosteve.api_gateway;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Main {
+	private static final Logger log = LoggerFactory.getLogger(Main.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
+		SpringApplication app = new SpringApplication(Main.class);
+		app.setLogStartupInfo(false);
+		app.setBannerMode(Banner.Mode.OFF);
+
+		var context = app.run(args);
+
+		String port = context.getEnvironment().getProperty("local.server.port");
+//		SpringApplication.run(Main.class, args);
+		log.info("-----------------------------------");
+		log.info("Service is running on port {}",port);
+		log.info("-----------------------------------");
 	}
 }
